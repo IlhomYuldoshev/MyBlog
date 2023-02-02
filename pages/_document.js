5c56271930925c15a1a7b5652f0ede9bfdc79c07
Ilhom <89073838+IlhomYuldoshev@users.noreply.github.com>
2023-02-02 17:26:46 +0500

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -32,7 +32,7 @@ export default function Document() {
           />
         <noscript>
             <div>
-                <img src="https://mc.yandex.ru/watch/92299612" style="position:absolute; left:-9999px;" alt="" />
+                <img src="https://mc.yandex.ru/watch/92299612" style={{position: 'absolute', left: '-9999px'}} alt="" />
             </div>
         </noscript>
 
